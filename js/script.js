fc1cfc828cf841322b83ae7754e334058561f4eb
inga-kapitonova <71716975+inga-kapitonova@users.noreply.github.com>
2021-01-12 18:02:12 +0200

    ja updated

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-localStorage.getItem('content');
-
 let form = $(".todo-form");
 
 /*new task div visibility*/
@@ -23,7 +21,7 @@ $(function () {
     $('.todo-form--btn').click(function () {
         let todoText = $('.todo-form--text').val();
 
-        $('<li class="listItem"><span contenteditable="true" class="listContent">' + todoText + '</span><a class="mark-done" href="#"><i class="fas fa-check"></i></a><a class="mark-delete" href="#"><i class="fas fa-trash-alt"></i></a></li>').prependTo('.listContainer');
+        $('<li class="listItem"><span contenteditable="true" class="listContent"><pre>' + todoText + '</pre></span><a class="mark-done" href="#"><i class="fas fa-check"></i></a><a class="mark-delete" href="#"><i class="fas fa-trash-alt"></i></a></li>').prependTo('.listContainer');
 
 
         $('.todo-form--text').val("");
